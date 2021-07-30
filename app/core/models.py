15060e77663bffc0ec404d003a528c68fdfc509c
frankJRT <frank_jr@live.com.mx>
2021-07-29 20:54:19 -0500

    crear recipientes

--- app/core/models.py
+++ app/core/models.py
@@ -1,36 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
                                         PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
+
 class UserManager(BaseUserManager):
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create and save new User"""
         if not email:
             raise ValueError('Usees must have email addres')
-        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,email, password=None):
+    def create_superuser(self, email, password=None):
         """Create and save new super User"""
         if not email:
             raise ValueError('Usees must have email addres')
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
 
 
-class User(AbstractBaseUser,PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """custom user model thah support using email listed of username"""
-    email = models.EmailField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-    USERNAME_FIELD ='email'
+    USERNAME_FIELD = 'email'
 
+
+class Tag(models.Model):
+    """Tag to be used for a recope"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
